f920a838e010901d1f48c3f24eb205ad8df8c0b2
alexmolnar <alex.molnar@lunatech.nl>
2026-09-13 10:26:24 +0200

    debug logging

--- backend/src/main/java/com/drinksaver/service/recommendations/PersistentPersonalRecommendationSource.java
+++ backend/src/main/java/com/drinksaver/service/recommendations/PersistentPersonalRecommendationSource.java
@@ -1,5 +1,6 @@
 package com.drinksaver.service.recommendations;
 
+import com.drinksaver.model.db.Recommendation;
 import com.drinksaver.repository.postgres.schema.RecommendationsTable;
 import com.drinksaver.service.model.DrinkKey;
 import com.drinksaver.service.recommendations.api.RecommendationSource;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -22,13 +24,18 @@ public class PersistentPersonalRecommendationSource implements RecommendationSou
 
     @Override
     public Map<DrinkKey, Double> buildRecommendation(UUID userId) {
-        return RecommendationsTable.findValidByUserId(
+        List<Recommendation> rec = RecommendationsTable.findValidByUserId(
             userId,
             LocalDateTime.now()
-        ).stream().collect(Collectors.toMap(
+        );
+        System.out.printf("Got number of recs: %d%n", rec.size());
+        rec.forEach(r -> System.out.printf("Rec: %s%n", r.getName()));
+        Map<DrinkKey, Double> ret = rec.stream().collect(Collectors.toMap(
             DrinkKey::of,
             notUsed -> Double.MAX_VALUE,
             (k1, k2) -> k1
         ));
+        ret.entrySet().forEach((e -> System.out.printf("K: %s, V: %f%n", e.getKey().name(), e.getValue())));
+        return ret; // TODO obviously
     }
 }
