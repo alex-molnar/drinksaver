310f28bb65da30e35fae8dce1de7b70fba746863
Alex Molnar <alex.molnar@ing.com>
2026-09-18 16:27:58 +0200

    Update api docs

--- backend/src/main/java/com/drinksaver/service/RecommendationService.java
+++ backend/src/main/java/com/drinksaver/service/RecommendationService.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.*;
 
 @Service
